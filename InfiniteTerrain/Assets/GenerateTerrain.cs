3484cb63c82e9e3bf8b3cb3115f432c599ad4288
ramish94 <ramish@ualberta.ca>
2016-03-15 09:45:41 -0600

    Flattend out the landscape. No more mountains. Now just the flattened version of the landscape appears

--- InfiniteTerrain/Assets/GenerateTerrain.cs
+++ InfiniteTerrain/Assets/GenerateTerrain.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class GenerateTerrain : MonoBehaviour {
 
-	int heightScale = 5;
-	float detailScale = 5.0f;
+	//int heightScale = 5;
+	//float detailScale = 5.0f;
 	// Use this for initialization
 	void Start () {
 		Mesh mesh = this.GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
-		for (int v = 0; v < vertices.Length; v++) {
+		/*for (int v = 0; v < vertices.Length; v++) {
 			vertices [v].y = Mathf.PerlinNoise ((vertices [v].x + this.transform.position.x) / detailScale,
 												(vertices [v].z + this.transform.position.z) / detailScale) * heightScale;
-		}
+		}*/
 
 		mesh.vertices = vertices;
 		mesh.RecalculateBounds ();
